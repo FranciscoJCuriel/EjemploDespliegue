01d76a6703323e3729993ab7e3a1561f2bddd395
Quarksoft <fjleal@quarksoft.net>
2023-11-11 17:23:51 -0600

    se agrega linea base

---
+++ sqlscripts/general.sql
@@ -0,0 +1,9 @@
+---------------------------------------------------------
+Autor:  Francisco Leal Curiel
+Empresa: Paco-Yair
+Descripción: creación de tablas
+---------------------------------------------------------
+
+CREATE Table ejemplo(
+	int id
+);
