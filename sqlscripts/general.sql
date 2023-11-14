45945bf1d8dc635d86294ce8473e9850d001f585
FranciscoJCuriel <111080118+FranciscoJCuriel@users.noreply.github.com>
2023-11-14 00:59:14 -0600

    Update general.sql

--- sqlscripts/general.sql
+++ sqlscripts/general.sql
@@ -1,9 +1,3 @@
----------------------------------------------------------
-Autor:  Francisco Leal Curiel
-Empresa: Paco-Yair
-Descripción: creación de tablas
----------------------------------------------------------
-
 CREATE Table ejemplo(
 	int id
 );
